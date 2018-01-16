349a3393d68917410c7a75e2d1391a0b799f7473
PresentKim <qjrmckd@gmail.com>
2018-01-17 02:24:36 +0900

    Use PoolCommand, Add reloadCommand()

--- src/presentkim/itempopup/ItemPopupMain.php
+++ src/presentkim/itempopup/ItemPopupMain.php
@@ -2,12 +2,12 @@
 
 namespace presentkim\itempopup;
 
-use pocketmine\command\{
-  CommandExecutor, PluginCommand
-};
 use pocketmine\plugin\PluginBase;
-use presentkim\itempopup\{
-  listener\PlayerEventListener, command\CommandListener, util\Translation
+use presentkim\itempopup\listener\PlayerEventListener;
+use presentkim\itempopup\util\Translation;
+use presentkim\itempopup\command\PoolCommand;
+use presentkim\itempopup\command\subcommands\{
+  SetSubCommand, RemoveSubCommand, ListSubCommand, LangSubCommand, ReloadSubCommand, SaveSubCommand
 };
 
 class ItemPopupMain extends PluginBase{
@@ -15,8 +15,8 @@ class ItemPopupMain extends PluginBase{
     /** @var self */
     private static $instance = null;
 
-    /** @var PluginCommand[] */
-    private $commands = [];
+    /** @var PoolCommand */
+    private $command;
 
     /** @return self */
     public static function getInstance(){
@@ -58,11 +58,7 @@ class ItemPopupMain extends PluginBase{
             Translation::load($langfilename);
         }
 
-        foreach ($this->commands as $command) {
-            $this->getServer()->getCommandMap()->unregister($command);
-        }
-        $this->commands = [];
-        $this->registerCommand(new CommandListener($this), Translation::translate('command-itempopup'), 'ItemPopup', 'itempopup.cmd', Translation::translate('command-itempopup@description'), Translation::translate('command-itempopup@usage'), Translation::getArray('command-itempopup@aliases'));
+        $this->reloadCommand();
     }
 
     public function save(){
@@ -74,26 +70,21 @@ class ItemPopupMain extends PluginBase{
         $this->saveConfig();
     }
 
-    /**
-     * @param CommandExecutor $executor
-     * @param                 $name
-     * @param                 $fallback
-     * @param                 $permission
-     * @param string          $description
-     * @param null            $usageMessage
-     * @param array|null      $aliases
-     */
-    private function registerCommand(CommandExecutor $executor, $name, $fallback, $permission, $description = "", $usageMessage = null, array $aliases = null){
-        $command = new PluginCommand($name, $this);
-        $command->setExecutor($executor);
-        $command->setPermission($permission);
-        $command->setDescription($description);
-        $command->setUsage($usageMessage ?? ('/' . $name));
-        if (is_array($aliases)) {
-            $command->setAliases($aliases);
+    public function reloadCommand(){
+        if ($this->command == null) {
+            $this->command = new PoolCommand($this, 'itempopup');
+            $this->command->createSubCommand(SetSubCommand::class);
+            $this->command->createSubCommand(RemoveSubCommand::class);
+            $this->command->createSubCommand(ListSubCommand::class);
+            $this->command->createSubCommand(LangSubCommand::class);
+            $this->command->createSubCommand(ReloadSubCommand::class);
+            $this->command->createSubCommand(SaveSubCommand::class);
         }
-
-        $this->getServer()->getCommandMap()->register($fallback, $command);
-        $this->commands[] = $command;
+        $this->command->updateTranslation();
+        $this->command->updateSudCommandTranslation();
+        if ($this->command->isRegistered()) {
+            $this->getServer()->getCommandMap()->unregister($this->command);
+        }
+        $this->getServer()->getCommandMap()->register(strtolower($this->getName()), $this->command);
     }
 }
